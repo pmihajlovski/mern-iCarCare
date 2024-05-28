1776bc47fb7c4c0ee129d86d0fbe60cb715a90dd
petar mihajlovski <petar.mihajlovski06@gmail.com>
2024-05-28 13:04:36 +0200

    bug images

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ const HomePage = () =>{
     <div className="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16">
       <div className="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-10">
         <div className="border-r border-b border-l border-gray-400 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col leading-normal">
-          <img src="./src/images/image1.jpg" className="object-fill h-48 w-96"/>
+          <img src="https://res.cloudinary.com/dwnc3l2ij/image/upload/v1716892009/ptnbwizedfhmfi2yrfje.jpg" className="object-fill h-48 w-96"/>
           <div className="p-4 pt-5 text-center">
               <p className="text-gray-900 font-bold text-xl mb-2">Ripariamo la tua auto</p>
               <p className="text-gray-700 text-sm">Da 50 anni l'officina di fiducia di molti italiani. Ci prendiamo cura della tua auto in tempi brevi.</p>
